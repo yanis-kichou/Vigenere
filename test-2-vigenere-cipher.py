833be3174039be858a48a62210efe415832c3339
Matthieu Lequesne <matthieu.lequesne@inria.fr>
2019-02-11 05:07:37 +0100

    improved test2

--- test-2-vigenere-cipher.py
+++ test-2-vigenere-cipher.py
@@ -9,6 +9,7 @@ print("Test chiffre_vigenere")
 assert chiffre_vigenere("ALICE",[0]) == "ALICE"
 assert chiffre_vigenere("ALICE",[3]) == "DOLFH"
 assert chiffre_vigenere("ALICE",[1,2,3]) == "BNLDG"
+assert chiffre_vigenere(read("data/text1.plain"),[10,9,7,0,24,22,0]) == read("data/text1.cipher")
 print("Test chiffre_vigenere : OK")
 
 print("---------------------")
@@ -17,6 +18,7 @@ print("Test dechiffre_cesar")
 assert dechiffre_vigenere("ALICE",[0]) == "ALICE"
 assert dechiffre_vigenere("DOLFH",[3]) == "ALICE"
 assert dechiffre_vigenere("BNLDG",[1,2,3]) == "ALICE"
+assert dechiffre_vigenere(read("data/text1.cipher"),[10,9,7,0,24,22,0]) == read("data/text1.plain")
 print("Test dechiffre_vigenere : OK")
 
 print("\n\n----------------------------------------------\n\n")
